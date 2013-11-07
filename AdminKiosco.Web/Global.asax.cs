570d02d4402e8bd2110dc1d5fc9d9174784cf0cf
dcohan <dcohan@vmbc.com>
2013-11-06 23:35:52 -0300

    Initial Commit

---
+++ AdminKiosco.Web/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.UI;
+using AdminKiosco.Web;
+
+namespace AdminKiosco.Web
+{
+    public class Global : HttpApplication
+    {
+  
+
+        private static void RegisterScripts()
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-1.8.2.min.js",
+                DebugPath = "~/Scripts/jquery-1.8.2.js",
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
+        }
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterOpenAuth();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterScripts();
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  Code that runs on application shutdown
+
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+        }
+    }
+}
