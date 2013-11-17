dd16f44088506c26333b102012ea29a54815ebdb
dcohan <dcohan@vmbc.com>
2013-11-17 19:45:37 -0300

    Added Error Handling (to eventloG) Added Error Page redirection

---
+++ AdminKiosco.Web/Error.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Error.aspx.cs" Inherits="AdminKiosco.Web.About" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <article>
+        <p>        
+            Oops, ha ocurrido un error!
+        </p>
+    </article>
+</asp:Content>
--- AdminKiosco.Web/Global.asax.cs
+++ AdminKiosco.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.DynamicData;
@@ -48,6 +49,37 @@ namespace AdminKiosco.Web
         {
             // Code that runs when an unhandled error occurs
 
+            // Get the exception object.
+            Exception exc = Server.GetLastError();
+
+            // Handle HTTP errors
+            if (exc.GetType() == typeof(HttpException))
+            {
+                // The Complete Error Handling Example generates
+                // some errors using URLs with "NoCatch" in them;
+                // ignore these here to simulate what would happen
+                // if a global.asax handler were not implemented.
+                if (exc.Message.Contains("NoCatch") || exc.Message.Contains("maxUrlLength"))
+                    return;
+
+                //Redirect HTTP errors to HttpError page
+                Server.Transfer("Error.aspx");
+            }
+
+            // For other kinds of errors give the user some information
+            // but stay on the default page
+            Response.Write("<h2>Global Page Error</h2>\n");
+            Response.Write(
+                "<p>" + exc.Message + "</p>\n");
+            Response.Write("Return to the <a href='Default.aspx'>" +
+                "Default Page</a>\n");
+
+            // Log the exception and notify system operators
+            //TODO IMPLEMENT SECURITY IN DATABASE!
+            EventLog.WriteEntry("Application", exc.Message + System.Environment.NewLine + exc.StackTrace);
+
+            // Clear the error from the server
+            Server.ClearError();
         }
     }
 }
